4c21fb9925510ab86896bc49f7d6e0286265f0ca
Guilherme Gonçalves <guilherme.goncalves@grupoboticario.com.br>
2022-07-04 14:11:29 -0300

    first schema and resolvers

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Estou on!')
+const { ApolloServer, gql} = require ('apollo-server')
+
+const typeDefs = gql`
+    type Query{
+        ola: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        ola() {
+            return "Bom dia!"
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({url}) => {
+    console.log(`Up on ${url}`)
+})
