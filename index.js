237bfe9baed1855cd79237d71ac100e765a40052
Guilherme Gonçalves <guilherme.goncalves@grupoboticario.com.br>
2022-07-06 14:25:53 -0300

    Usuario query and resolver

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const { ApolloServer, gql} = require ('apollo-server')
 
 const typeDefs = gql`
+    scalar Date
+
+    type Usuario{
+        id: ID!
+        nome: String
+        salario: Float
+        vip: Boolean
+    }
+    
+
     type Query{
         ola: String
+        horaAtual: Date
+        usuarioLogado: Usuario
     }
 `
 
 const resolvers = {
+    Usuario: {
+        salario(usuario){
+            console.log(usuario)
+            return usuario.salario_real
+        }        
+    },    
     Query: {
         ola() {
             return "Bom dia!"
+        },
+        horaAtual() {
+            return `${new Date()}`
+        },
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: "Fulano de tal",
+                salario_real: 5240.20,
+                vip: false
+            }
         }
     }
 }
