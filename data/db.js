ee21076c26b42d85f8d246be7f3d30e555bcdd2a
Guilherme Gonçalves <guilherme.goncalves@grupoboticario.com.br>
2022-07-18 16:44:31 -0300

    enum of Usuario

--- data/db.js
+++ data/db.js
@@ -4,21 +4,24 @@ const usuarios = [
         nome: "Fulano de tal",
         salario_real: 5240.20,
         vip: false,
-        perfil_id: 1
+        perfil_id: 1,
+        status: 'ATIVO'
     },
     {
         id: 2,
         nome: "Beltrano Silva",
         salario_real: 10541.30,
         vip: true,
-        perfil_id: 2
+        perfil_id: 2,
+        status: 'INATIVO'
     },
     {
         id: 3,
         nome: "Ciclano Pereira",
         salario_real: 2032.30,
         vip: false,
-        perfil_id: 1
+        perfil_id: 1,
+        status: 'BLOQUEADO'
     }
 ]
 
